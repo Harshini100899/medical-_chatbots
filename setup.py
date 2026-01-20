fd6eaa147b177a016de14a15c6dd04990bced335
Harshini100899 <hershyeggoni@gmail.com>
2026-01-20 20:52:27 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_chatbots',
+    version='0.1.0',
+    author= "Harshini Eggoni",
+    author_email="hershyeggoni@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
